1762c7257393c7ec145c636be26125f88b136bcd
Muhammad Muntasir Mahamud Ashif <muntasirashifee@gmail.com>
2025-05-06 14:52:56 +0600

    Update stripe_key.dart

--- lib/core/services/stripe_services/stripe_key.dart
+++ lib/core/services/stripe_services/stripe_key.dart
@@ -1,6 +1,6 @@
 class StripeKey{
-  final String _stripePublishableKey = "pk_test_51R7YCuFKXEtfT5CGd9p48VbqDaf5azpaHB4rada6f7AzL7eTXG00GNPsRcPOQDdyLTduywLmHc9cUSm3QkAwzXa400zTadXSnn";
-  final String _stripeSecurityKey = "sk_test_51R7YCuFKXEtfT5CGLl9dKXXltRZd7ViTvlsZhFiP3EoZpRYyObXEC5RSP3Gdv0d3L6K9ZiTTuAU0pzkZIR6GSRqy00AiXHJqWX";
+  final String _stripePublishableKey = "";
+  final String _stripeSecurityKey = "";
 
   String get stripeSecurityKey => _stripeSecurityKey;
   String get stripePublicKey => _stripePublishableKey;
